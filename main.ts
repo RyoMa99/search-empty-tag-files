dc1ee9deb49940c04726b8c344aebcbb87511360
rym49 <hatanakaryoma.130@gmail.com>
2025-06-15 16:24:41 +0900

    init

--- main.ts
+++ main.ts
@@ -16,6 +16,9 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		// プラグイン有効化時にhello worldを表示
+		new Notice('hello world');
+
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
